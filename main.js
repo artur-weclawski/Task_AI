a4a40240f991527f37cb3e658cb2b37d6dbf9e7a
Artur Weclawski <weclawski.artur@gmail.com>
2024-11-14 17:03:05 +0100

    Added template

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function loadDataFromFile(filePath){
     try{
         return fs.readFileSync(filePath, 'utf8');
     } catch (error){
-        console.log(error)
+        console.log("Loading data error: ", error)
         return;
     }
 }
@@ -17,7 +17,7 @@ function saveResponseToFile(filePath, response){
     try{
         fs.writeFileSync(filePath, response, 'utf8')
     } catch (error){
-        console.log(error)
+        console.log("Saving to file error: ", error)
         return;
     }
 }
@@ -34,12 +34,14 @@ if(!userContent){
             { role: 'system', content: 
                 `
                 Na podstawie dostarczonego pliku tekstowego wygeneruj artykuł w postaci HTML.
+                - Wygenerowany artykuł powienien być w tym samym języku co dostarczony plik tekstowy.
                 - Nie używaj bloków kodu (backticków).
                 - Użyj odpowiednich tagów HTML do struktyzacji treści.
                 - W kodzie strony uwzględnij miejsce na grafiki (zdecyduj gdzie najlepiej je umieścić):
                     - oznacz je tagiem <img> z atrybutem src="image_placeholder.jpg",
                     - dodaj do każdego <img> atrybut alt, w którym umieścisz prompt za pomocą którego można wygenerować grafikę,
-                    - do każdej grafiki umieść podpis pod samą grafiką używając tagu <figcaption>.
+                    - do każdej grafiki umieść podpis pod samą grafiką używając tagu <figcaption>,
+                    - obraz jak i podpis pod grafiką zawrzyj w znaczniku <figure>.
                 - Nie generuj kodu CSS ani JavaScript.
                 - Zwrócony kod ma zawierać jedynie zawartość <body> BEZ SAMEGO ZNACZNIKA <body>, tylko zawartość, która by się tam znalazła.
                 - Nie dołączaj znaczników <html>, <head>.
